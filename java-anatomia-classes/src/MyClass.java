6d40b573060377383c2fd408d2f0a23e7d0bf374
Deltta <deltta.dev@gmail.com>
2024-09-26 22:16:23 -0300

    estudos 26.09

--- java-anatomia-classes/src/MyClass.java
+++ java-anatomia-classes/src/MyClass.java
@@ -2,8 +2,18 @@ public class MyClass {
 
     public static void main (String [] args){
 
-        System.out.print("Hello, World!");
-        
+        String primeroNome = "Gustavo";
+        String segundoNome = "Sousa";
+
+        // Variável que inicia e já recebe o resultado do método.
+        String nomeCompleto = nomeCompleto(primeroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
+    }
+
+    // Método que concatena o meu primeiro e segundo nome.
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
